72002ee28229bb58df83c7c2684468c68f0137e8
Himanshu <himanshuguptams@gmail.com>
2018-01-09 20:06:55 -0800

    7 commit

--- JavaFundamentals/src/practiceJavaPrograms/VarArgsMethod.java
+++ JavaFundamentals/src/practiceJavaPrograms/VarArgsMethod.java
@@ -1,17 +1,18 @@
 package practiceJavaPrograms;
 
 public class VarArgsMethod {
-	public static void m1(int... args) {
-		System.out.println("var-arg method");
-
-	}
 	public static void main(String[] args) {
-		m1();
-		m1(10);
-		m1(10,20);
-		m1(10,20,30,40);
-//		m1(hi);
+		sum(); //0
+		sum(10); //10
+		sum(10,20); //30
+		sum(10,20,30,40); //100
 		
 	}
-
+	public static void sum(int  ...  x) {
+		int total = 0;
+		for (int i = 0; i < x.length; i++) {
+			total += x[i];
+		}
+		System.out.println("The sum is: "+total);
+	}
 }
