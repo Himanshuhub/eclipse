283fdde77f7fbcd115bbf233940a1c2d52c039fa
Himanshu <himanshuguptams@gmail.com>
2018-01-11 17:44:28 -0800

    8

---
+++ JavaFundamentals/src/practiceJavaPrograms/CLA.java
@@ -0,0 +1,5 @@
+package practiceJavaPrograms;
+
+public class CLA {
+
+}
--- JavaFundamentals/src/practiceJavaPrograms/MainMethod.java
+++ JavaFundamentals/src/practiceJavaPrograms/MainMethod.java
@@ -1,6 +1,22 @@
 package practiceJavaPrograms;
 
 public class MainMethod {
-	
+	public static void main(String[] args) {
+		m10();
+		System.out.println("main method");
+	}
+	private static void m10() {
+		// TODO Auto-generated method stub
+		System.out.println("print String method");
+	}
+	public static void main(int[] args) {
+		System.out.println("print int method");
+	}
+}
 
+class child extends MainMethod {
+	public static void main(String[] args) {
+		System.out.println("child class");
+	}
+	
 }
