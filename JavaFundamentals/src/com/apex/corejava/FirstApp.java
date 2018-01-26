1ba10cee1f139f75db91ae9c486a63651c4471c8
Himanshu <himanshuguptams@gmail.com>
2018-01-26 12:55:37 +0530

    10

---
+++ JavaFundamentals/src/com/apex/corejava/FirstApp.java
@@ -0,0 +1,11 @@
+package com.apex.corejava;
+
+public class FirstApp { //class is Keyword
+
+	public static void main(String[] args) { //main method i sstarting point for our application execution
+		// TODO Auto-generated method stub
+		System.out.println("Firt Java App1");
+
+	}
+
+}
